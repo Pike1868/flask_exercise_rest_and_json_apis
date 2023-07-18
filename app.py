0a6a500b5b4a9db2137b38035289fb921c3861da
Pike1868 <luis_14mendoza@protonmail.com>
2023-07-18 14:35:35 -0400

    Added Part 2 and 3: Routes for listing, getting, creating, updating and deleting cupcakes

--- app.py
+++ app.py
@@ -10,10 +10,50 @@ app.config['SECRET_KEY'] = 'secret-key'
 
 connect_db(app)
 
-@app.route("/cupcakes")
+@app.route("/api/cupcakes")
 def list_all_cupcakes():
     """Return all cupcakes"""
     
     all_cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
     
     return jsonify(all_cupcakes)
+
+@app.route("/api/cupcakes/<int:id>")
+def list_cupcake_details(id):
+    """Get data about a single cupcake, respond with JSON"""
+    cupcake = Cupcake.query.get_or_404(id)
+     
+    return jsonify(cupcake.serialize())
+
+@app.route("/api/cupcakes", methods=["POST"])
+def create_cupcake():
+    """Create a cupcake with flavor, size, rating and image data from the body of the request. Respond with JSON like: {cupcake: {id, flavor,size, rating, image}}."""
+    
+    new_cupcake = Cupcake(flavor=request.json['flavor'], size=request.json['size'], rating=request.json['rating'], image=request.json['image'])
+    
+    db.session.add(new_cupcake)
+    db.session.commit()
+    
+    return (jsonify(new_cupcake=new_cupcake.serialize()), 201)
+
+@app.route("/snacks/<int:id>", methods=["PATCH"])
+def update_cupcake(id):
+    cupcake = Cupcake.query.get_or_404(id)
+    
+    cupcake.flavor = request.json.get('flavor', cupcake.flavor)
+    cupcake.size = request.json.get('size', cupcake.size)
+    cupcake.rating = request.json.get('rating', cupcake.rating)
+    cupcake.image = request.json.get('image', cupcake.image)
+    
+    db.session.commit()
+    
+    return (jsonify(cupcake=cupcake.serialize()))
+
+
+@app.route("/snacks/<int:id>", methods=["DELETE"])
+def delete_cupcake(id):
+    cupcake = Cupcake.query.get_or_404(id)
+    
+    db.session.delete(cupcake)
+    
+    return jsonify(message="Deleted")
